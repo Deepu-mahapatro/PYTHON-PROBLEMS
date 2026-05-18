5ecfabff46ae8d14fe28eed84a9784cb2ba638d0
deepu <deepumahapatro2006@gmail.com>
2026-05-18 09:20:02 +0530

    PALINDROME CHECKER

--- PROBLEM5.py
+++ PROBLEM5.py
@@ -1,6 +1,7 @@
 #PALINDROME NUMBER
 
-num=int(input("enter a number"))
+#FOR INTEGER
+num=int(input("enter a number:  "))
 original=num
 sum=0
 while num>0:
@@ -8,6 +9,15 @@ while num>0:
     sum=sum*10+rem
     num=num//10
 if sum==original:
+    print("palindrome")
+else:
+    print("not a palindrome")
+    
+
+#FOR STRING
+str=input("enter  string:  ")
+result=str[::-1]
+if str==result:
     print("palindrome")
 else:
     print("not a palindrome")
