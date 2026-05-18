1a7f84c2f18fddca8e653621e085924cb266ff92
deepu <deepumahapatro2006@gmail.com>
2026-05-18 10:00:46 +0530

    PALINDROME CHECK OF A NUMBER

--- PROBLEM5.py
+++ PROBLEM5.py
@@ -2,6 +2,7 @@
 
 #FOR INTEGER
 num=int(input("enter a number:  "))
+num=abs(num)
 original=num
 sum=0
 while num>0:
@@ -14,10 +15,44 @@ else:
     print("not a palindrome")
     
 
-#FOR STRING
-str=input("enter  string:  ")
-result=str[::-1]
-if str==result:
+#USING STRING METHOD
+text=int(input("enter  string:  "))
+text=str(text)
+result=text[::-1]
+if text==result:
     print("palindrome")
 else:
     print("not a palindrome")
+    
+#RECURSION METHOD
+def prob(num,reverse=0,original=None):
+    if original is None:
+        original=num
+    if num==0:
+        return original==reverse
+    rem=num%10
+    reverse=reverse*10+rem
+    return prob(num//10,reverse,original)
+num=int(input("enter a number"))
+if prob(abs(num)):
+    print("palindrome")
+else:
+    print("not a palindrome")
+    
+
+#UsiNG RECURSION ANOTHER FORMAT
+def prob(num, reverse=0):
+
+    num = abs(num)
+    original=num
+    if num == 0:
+        return reverse
+    rem = num % 10
+    reverse = reverse * 10 + rem
+    return prob(num // 10, reverse)
+num = int(input("Enter a number: "))
+if original == prob(num):
+    print("palindrome")
+else:
+    print("not palindrome")
+    
