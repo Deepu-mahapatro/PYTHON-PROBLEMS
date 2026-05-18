f83b14d2374e08f37e75d74e648b5155a5bd410d
deepu <deepumahapatro2006@gmail.com>
2026-05-18 10:16:49 +0530

    REVERSED A NUMBER

--- PROBLEM4.py
+++ PROBLEM4.py
@@ -1,9 +1,29 @@
 #REVERSE A NUMBER
 
 nums=int(input("enter a number"))
+sign=-1 if nums<0 else 1
+nums=abs(nums)
 reverse=0
 while nums>0:
     rem=nums%10
     reverse=reverse*10+rem
     nums=nums//10
-print("reverse of a number:",reverse)
+print("reverse of a number:",reverse*sign)
+
+#USING STRING SLICING
+num=int(input("enter a number"))
+sign=-1 if num<0 else 1
+reverse=int(str(abs(num))[::-1])
+print("reversed number :",reverse*sign)
+
+#USING RECURSION METHOD
+def prob(num,reverse=0):
+    if num==0:
+        return reverse
+    rem=num%10
+    reverse=reverse*10+rem
+    return prob(num//10,reverse)
+num=int(input("enter a number"))
+sign=-1 if num<0 else 1
+result=prob(abs(num))
+print("reversed number:",result*sign)
