d0e32275aeb93db01cace94cfd454031e1561e7f
deepu <deepumahapatro2006@gmail.com>
2026-05-18 09:42:41 +0530

    SUM OF DIGITS

--- PROBLEM6.py
+++ PROBLEM6.py
@@ -1,11 +1,24 @@
 #SUM OF DIGITS OF A NUMBER
 
+#USING WHILE LOOP METHOD
+
 num=int(input("enter a number"))
+num=abs(num)
 sum=0
 while num>0:
     rem=num%10
     sum+=rem
     num=num//10
 print("sum of digits: ",sum)
+
+#USING RECURSION METHOD
+
+def prob(nums):
+    nums=abs(nums)
+    if nums==0:
+        return 0
+    return (nums%10) +prob(nums//10)
+nums=int(input("enter a number"))
+print("sum of digits:",prob(nums))
     
     
